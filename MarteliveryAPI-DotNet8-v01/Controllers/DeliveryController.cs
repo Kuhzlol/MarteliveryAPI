49eab5cfdd4fb60f28c809f48e01d23023282aa8
Kuhzlol <romainledda@hotmail.com>
2024-01-04 18:31:14 +0100

    Creation of controllers

---
+++ MarteliveryAPI-DotNet8-v01/Controllers/DeliveryController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarteliveryAPI_DotNet8_v01.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class DeliveryController : ControllerBase
+    {
+    }
+}
